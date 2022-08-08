a4e81a7e695d15d66c6cf1d3c40a3f8e1d148266
Jr Bytes <itamazor@gmail.com>
2022-08-08 14:48:18 -0300

    :tada:

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.green500};
+  }
+
+  body {
+    ${({ theme }) => `
+      background-color: ${theme.gray900};
+      color: ${theme.gray300};
+    `}
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Calibri', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
