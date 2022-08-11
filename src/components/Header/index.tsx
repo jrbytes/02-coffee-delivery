90e9dbb6937670473b154777bc40e3b5a2fc082c
Jr Bytes <itamazor@gmail.com>
2022-08-11 10:08:15 -0300

    adicionado funcionalidade que atualiza quantidade de itens ao cabecalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,14 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 
 import { AmountQuantity, HeaderContainer, Location } from './styles'
 import logo from '../../assets/logo.svg'
+import { CartContext } from '../../contexts/CartContext'
+import { useContext } from 'react'
 
 export function Header() {
+  const { cart } = useContext(CartContext)
+
+  const amountProductTotal = !cart ? '0' : cart.items.length
+
   return (
     <HeaderContainer>
       <img src={logo} alt="Logo do Coffee Delivery" />
@@ -15,7 +21,7 @@ export function Header() {
         </span>
         <button>
           <ShoppingCart weight="fill" size={20} />
-          <AmountQuantity>3</AmountQuantity>
+          <AmountQuantity>{amountProductTotal}</AmountQuantity>
         </button>
       </Location>
     </HeaderContainer>
